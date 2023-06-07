f7d47070c25316fd48ca0ff2142d08de961caee7
dzvoti <libertydzvotimlambo@gmail.com>
2023-06-07 16:54:48 +0100

    refactor: Rename standard_name_mappings to standard_name_mappings_pairs
    Renamed the `standard_name_mappings` data frame to `standard_name_mappings_pairs` in the `data.R` file to better reflect the contents of the data frame.
    
    #git.commit#

--- R/data.R
+++ R/data.R
@@ -2,4 +2,4 @@
 #' @name standard_name_mappings_pairs
 #' @title Standard HCES Variable Name Mappings
 #' @description A data frame containing standard variable name mapping pairs for the IHS5 survey.
-"standard_name_mappings_pairs" <- read.csv(system.file("data", "standard_name_mappings_pairs.csv", package = "hcesR2"))
+"standard_name_mappings_pairs" <- read.csv("data/standard_name_mappings_pairs.csv")
