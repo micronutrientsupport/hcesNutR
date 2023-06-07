6dafe3621378f70bc56f9f891e6d0b25831423cf
dzvoti <libertydzvotimlambo@gmail.com>
2023-06-07 17:38:30 +0100

    feat: Add functionality to check for changed columns in HCES data
    Updated the `check_hces_names` function to check for columns that have name changes in the HCES data file compared to theoriginal input data. It works inside the `rename_hces` function by comparing the standard MAPS column names to those in the input file. The function now prints the changes to the console and returns a dataframe.
    
    #git.commit#

--- R/clean_hces.R
+++ R/clean_hces.R
@@ -14,3 +14,34 @@ get_hces_mappings <- function(country_name, survey_name) {
 
     return(column_mapping)
 }
+check_hces_names <- function(data, original_data_df) {
+    #' Compare column names in HCES data
+    #'
+    #' @description This function compares the column names in the hces data file to the standard MAPS column names. It print the changes to the console.
+    #' @param data dataframe . The data file as a dataframe.
+    #' @param original_data_df dataframe . The original data file as a dataframe.
+    #' @return dataframe: The data file with renamed columns, converted data types and a table of the changes to the console.
+    #' @export
+    #' 
+    # Load original column names into a dataframe
+    original_colnames <- colnames(original_data_df)
+
+    # Load renamed column names into a dataframe
+    renamed_colnames <- colnames(data)
+
+    # Check for columns that have been renamed
+    changed_cols <- setdiff(renamed_colnames, original_colnames)
+
+    # Print the changes to the console
+    if (length(changed_cols) > 0) {
+        changes_df <- data.frame(hces_standard_name = NULL, survey_variable_names = NULL)
+        for (i in 1:length(changed_cols)) {
+            changes_df <- rbind(changes_df, data.frame(hces_standard_name = changed_cols[i], survey_variable_names = original_colnames[match(changed_cols[i], renamed_colnames)]))
+        }
+        message(cat("\n...\nThe following columns were renamed: \n"))
+        print(changes_df)
+    } else {
+        message(cat("No columns were renamed."))
+    }
+    return(data)
+}
