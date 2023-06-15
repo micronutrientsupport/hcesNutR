76ed9505041815c1d03b7fc1690cf47a0af0eb8c
dzvoti <libertydzvotimlambo@gmail.com>
2023-06-15 11:04:16 +0100

    feat:Add `standard_food_list` tibble
    This commit adds the `standard_food_list` data frame to the `data.R` file. The tibble contains food names and codes of surveys supported by this package, and is read in from the "standard_food_list.csv" file using the `readr::read_csv()` function. Additionally, the commit adds documentation for the data frame using roxygen2-style comments. This change was made to provide internal functions with a standardized list of food names and codes for use in their analyses.

--- R/data.R
+++ R/data.R
@@ -4,3 +4,10 @@
 #' @description A data frame containing standard variable name mapping pairs for the IHS5 survey.
 "standard_name_mappings_pairs" <- readr::read_csv(here::here("data","standard_name_mappings_pairs.csv"))
 
+
+#' @docType data
+#' @name standard_food_list
+#' @title Standard Food Names and Codes
+#' @description A data frame containing food names and codes of surveys supported by this package.
+"standard_food_list" <- readr:::read_csv(here::here("data","standard_food_list.csv"))
+
