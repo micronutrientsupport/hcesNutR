3f5deb84849c02e089ba765292ff4810c791fbfc
dzvoti <libertydzvotimlambo@gmail.com>
2023-07-24 11:34:31 +0100

    chore: update `data.r` to suppress warnings and messages when loading internal data

--- R/data.R
+++ R/data.R
@@ -2,18 +2,18 @@
 #' @name standard_name_mappings_pairs
 #' @title Standard HCES Variable Name Mappings
 #' @description A data frame containing standard variable name mapping pairs for the IHS5 survey.
-"standard_name_mappings_pairs" <- readr::read_csv(here::here("data","standard_name_mappings_pairs.csv"))
+"standard_name_mappings_pairs" <- suppressMessages(readr::read_csv(here::here("data","standard_name_mappings_pairs.csv")))
 
 
 #' @docType data
 #' @name standard_food_list
 #' @title Standard Food Names and Codes
 #' @description A data frame containing food names and codes of surveys supported by this package.
-"standard_food_list" <- readr:::read_csv(here::here("data","standard_food_list.csv"))
+"standard_food_list" <- suppressMessages(readr:::read_csv(here::here("data","standard_food_list.csv")))
 
 
 #' @docType data
 #' @name non_standard_food_list
 #' @title Non Standard Food Names and Codes
 #' @description A data frame containing non-standard food names and codes of surveys supported by this package.
-"non_standard_food_list" <- readr:::read_csv(here::here("data","non_standard_food_list.csv"))
+"non_standard_food_list" <- suppressMessages(readr:::read_csv(here::here("data","non_standard_food_list.csv")))
