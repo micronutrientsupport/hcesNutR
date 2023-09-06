f146152f79c7e027395d0503b6950ac44ee130f9
dzvoti <libertydzvotimlambo@gmail.com>
2023-09-06 11:19:45 +0100

    update: Update data.R
    This changes the internal data handling from loading .csv files with readr to loading .rds files with readRDS

--- R/data.R
+++ R/data.R
@@ -2,24 +2,24 @@
 #' @name standard_name_mappings_pairs
 #' @title Standard HCES Variable Name Mappings
 #' @description A data frame containing standard variable name mapping pairs for the IHS5 survey.
-"standard_name_mappings_pairs" <- suppressMessages(readr::read_csv(here::here("data","standard_name_mappings_pairs.csv")))
+"standard_name_mappings_pairs" <- suppressMessages(readRDS(here::here("data","standard_name_mappings_pairs.rds")))
 
 
 #' @docType data
 #' @name standard_food_list
 #' @title Standard Food Names and Codes
 #' @description A data frame containing food names and codes of surveys supported by this package.
-"standard_food_list" <- suppressMessages(readr:::read_csv(here::here("data","standard_food_list.csv")))
+"standard_food_list" <- suppressMessages(readRDS(here::here("data","standard_food_list.rds")))
 
 
 #' @docType data
 #' @name non_standard_food_list
 #' @title Non Standard Food Names and Codes
 #' @description A data frame containing non-standard food names and codes of surveys supported by this package.
-"non_standard_food_list" <- suppressMessages(readr:::read_csv(here::here("data","non_standard_food_list.csv")))
+"non_standard_food_list" <- suppressMessages(readRDS(here::here("data","non_standard_food_list.rds")))
 
 #' @docType data
 #' @name combined_food_list
 #' @title Food Names and Codes Supported by this Package
 #' @description A data frame containing both standard and non-standard food names and codes of surveys supported by this package.
-"combined_food_list" <- suppressMessages(readr:::read_csv(here::here("data","combined_food_list_v26072023.csv")))
+"combined_food_list" <- suppressMessages(readRDS(here::here("data","combined_food_list_v26072023.rds")))
