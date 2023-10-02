bb00147e6c0c04223cd790699ce12df859ea0d37
dzvoti <libertydzvotimlambo@gmail.com>
2023-10-02 09:47:25 +0100

    chore: Linting changes to `data.R`

--- R/data.R
+++ R/data.R
@@ -2,21 +2,21 @@
 #' @name standard_name_mappings_pairs
 #' @title Standard HCES Variable Name Mappings
 #' @description A data frame containing standard variable name mapping pairs for the IHS5 survey.
-"standard_name_mappings_pairs" <- suppressMessages(readRDS(here::here("data","standard_name_mappings_pairs.rds")))
+"standard_name_mappings_pairs" <- suppressMessages(readRDS(here::here("data", "standard_name_mappings_pairs.rds")))
 
 
 #' @docType data
 #' @name standard_food_list
 #' @title Standard Food Names and Codes
 #' @description A data frame containing food names and codes of surveys supported by this package.
-"standard_food_list" <- suppressMessages(readRDS(here::here("data","standard_food_list.rds")))
+"standard_food_list" <- suppressMessages(readRDS(here::here("data", "standard_food_list.rds")))
 
 
 #' @docType data
 #' @name non_standard_food_list
 #' @title Non Standard Food Names and Codes
 #' @description A data frame containing non-standard food names and codes of surveys supported by this package.
-"non_standard_food_list" <- suppressMessages(readRDS(here::here("data","non_standard_food_list.rds")))
+"non_standard_food_list" <- suppressMessages(readRDS(here::here("data", "non_standard_food_list.rds")))
 
 #' @docType data
 #' @name combined_food_list
